befd20d5459e57c89d018df69c0b987285c38a28
lucas Augusto <lucas.fernandes0522@outlook.com>
2021-02-27 10:59:09 -0300

    Notificações sendo recebidas

---
+++ Source/Mobile/Notifiquei/Notifiquei.Mobile/Notifiquei.Mobile/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notifiquei.Mobile
+{
+    public partial class App : Application
+    {
+        public App(string token)
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage(token);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
